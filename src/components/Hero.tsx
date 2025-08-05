c4d24094bfc65ea56eeb81564493d60c93d53f76
SuperMindTeam <hello@getsupermind.com>
2025-08-05 22:59:41 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -118,7 +118,7 @@ const Hero = () => {
           >
             <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center mb-12 max-w-7xl mx-auto">
               <div className="text-left max-w-none">
-                <h1 className="text-6xl md:text-7xl lg:text-8xl font-sanomat font-semibold tracking-tight mt-4 mb-6 text-white max-w-8xl">
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-sanomat font-semibold tracking-tight mt-4 mb-6 text-white max-w-12xl">
                   Cut Costs<br />Calls Answered 24 hours
                 </h1>
                 
