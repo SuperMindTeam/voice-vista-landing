81c8072de7f6eff3c959e3ebe882c6b45a5af02c
SuperMindTeam <hello@getsupermind.com>
2025-08-05 23:49:42 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,8 +43,8 @@ const Hero = () => {
         <img 
           src="/lovable-uploads/HandwrittenMic.svg" 
           alt="Handwritten mic" 
-          className="absolute bottom-0 right-0 w-380 h-380 text-white"
-          style={{ filter: 'brightness(0) invert(1)' }}
+          className="absolute bottom-0 right-0"
+          style={{ width: '380px', height: '380px', filter: 'brightness(0) invert(1)' }}
         />
         {/* Hand-drawn style arrow */}
         <svg 
