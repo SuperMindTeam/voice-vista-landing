d1dfc17bb9a8dc7f5fd0fd02cf02d53b58da4901
SuperMindTeam <hello@getsupermind.com>
2025-08-07 00:55:54 +0100

    Update VapiMicButton.tsx

--- src/components/VapiMicButton.tsx
+++ src/components/VapiMicButton.tsx
@@ -144,7 +144,7 @@ const VapiMicButton: React.FC<VapiMicButtonProps> = ({ className, assistantId })
             className="w-24 h-24 text-white"
             fill="currentColor" 
             viewBox="0 0 24 24"
-            style={{ width: '96px', height: '96px' }}
+            style={{ width: '90px', height: '90px' }}
           >
             <path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z"/>
           </svg>
