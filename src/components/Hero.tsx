bb569192943affa9e0976eef102c709237778a20
SuperMindTeam <hello@getsupermind.com>
2025-08-06 01:16:54 +0100

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -38,7 +38,7 @@ const Hero = () => {
   const CallToAction = () => (
   <div className="relative mt-8 w-full flex justify-center">
     {/* New gradient SVG positioned on the right side */}
-    <div className="absolute right-8 -translate-y-94 translate-x-14">
+    <div className="absolute right-8 -translate-y-94">
       <img 
         src="/lovable-uploads/d16122e6-44c7-4082-ba99-a318c5e3213c.svg" 
         alt="Gradient mic" 
